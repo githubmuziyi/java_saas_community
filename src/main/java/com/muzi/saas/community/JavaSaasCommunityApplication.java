e2571b00998da7700e7f9454dc1565e42dc374e5
muzi.he <muzi.habo@gmail.com>
2017-11-26 23:06:18 +0800

    社区服务框架搭建

---
+++ src/main/java/com/muzi/saas/community/JavaSaasCommunityApplication.java
@@ -0,0 +1,12 @@
+package com.muzi.saas.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSaasCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSaasCommunityApplication.class, args);
+	}
+}
